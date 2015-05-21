dc9905ed6d1a8c40fcc368df924d4335bc5f5cc0
nickman <whitehead.nicholas@gmail.com>
2015-05-21 08:04:21 -0400

    DevSynch

---
+++ src/main/java/com/heliosapm/utils/relfect/MetaHelper.java
@@ -0,0 +1,71 @@
+/**
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+ */
+package com.heliosapm.utils.relfect;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.heliosapm.utils.lang.StringHelper;
+
+/**
+ * <p>Title: MetaHelper</p>
+ * <p>Description: Helper to perform common relfective lookups against classes</p> 
+ * <p>Company: Helios Development Group LLC</p>
+ * @author Whitehead (nwhitehead AT heliosdev DOT org)
+ * <p><code>com.heliosapm.utils.relfect.MetaHelper</code></p>
+ */
+
+public class MetaHelper {
+
+	/**
+	 * Finds all the methods in the passed class that are annotated with the passed annotation
+	 * and is enabled for any of the passed modifiers
+	 * @param target  The class to inspect
+	 * @param annotation The annotation that must be on a matching method
+	 * @param modifiers The modifiers the method must be enabled for any of
+	 * @return an array of matching methods
+	 */
+	public static Method[] findAnnotatedMethods(final Class<?> target, final Class<? extends Annotation> annotation, final SModifier...modifiers) {
+		if(target==null) throw new IllegalArgumentException("The passed target was null");
+		if(annotation==null) throw new IllegalArgumentException("The passed annotation was null");
+		final Map<String, Method> matchingMethods = new HashMap<String, Method>();
+		for(Method m: target.getMethods()) {
+			if(m.getAnnotation(annotation)!=null) {
+				if(SModifier.isEnabledForAny(m, modifiers)) {
+					matchingMethods.put(StringHelper.getMethodDescriptor(m), m);
+				}
+			}
+		}
+		for(Method m: target.getDeclaredMethods()) {
+			if(m.getAnnotation(annotation)!=null) {
+				if(SModifier.isEnabledForAny(m, modifiers)) {
+					matchingMethods.put(StringHelper.getMethodDescriptor(m), m);
+				}
+			}
+		}
+		return matchingMethods.values().toArray(new Method[matchingMethods.size()]);
+	}
+	
+	
+	public MetaHelper() {}
+
+}
---
+++ src/main/java/com/heliosapm/utils/relfect/SModifier.java
@@ -0,0 +1,131 @@
+/**
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+ */
+package com.heliosapm.utils.relfect;
+
+import java.lang.reflect.Member;
+import java.lang.reflect.Modifier;
+
+/**
+ * <p>Title: SModifier</p>
+ * <p>Description: Enum version of java's reflection Modifier class</p> 
+ * <p>Company: Helios Development Group LLC</p>
+ * @author Whitehead (nwhitehead AT heliosdev DOT org)
+ * <p><code>com.heliosapm.utils.relfect.SModifier</code></p>
+ */
+
+public enum SModifier {
+	/** The public modifier */
+	PUBLIC(Modifier.PUBLIC),
+	/** The private modifier */
+	PRIVATE(Modifier.PRIVATE),
+	/** The protected modifier */
+	PROTECTED(Modifier.PROTECTED),
+	/** The static modifier */
+	STATIC(Modifier.STATIC),
+	/** The final modifier */
+	FINAL(Modifier.FINAL),
+	/** The synchronized modifier */
+	SYNCHRONIZED(Modifier.SYNCHRONIZED),
+	/** The volatile modifier */
+	VOLATILE(Modifier.VOLATILE),
+	/** The transient modifier */
+	TRANSIENT(Modifier.TRANSIENT),
+	/** The native modifier */
+	NATIVE(Modifier.NATIVE),
+	/** The interface modifier */
+	INTERFACE(Modifier.INTERFACE),
+	/** The abstract modifier */
+	ABSTRACT(Modifier.ABSTRACT),
+	/** The strict modifier */
+	STRICT(Modifier.STRICT);
+	
+	private SModifier(final int mask) {
+		_mask = mask;
+	}
+	
+	/** The modifier mask */
+	public final int _mask;
+	
+	/**
+	 * Returns a mask enabled for all the passed modifiers
+	 * @param modifiers The modifiers to get a mask for
+	 * @return the mask
+	 */
+	public static int getMaskFor(final SModifier...modifiers) {
+		int mask = 0;
+		for(SModifier m: modifiers) {
+			if(m==null) continue;
+			mask = mask | m._mask;
+		}
+		return mask;
+	}
+	
+	/**
+	 * Determines if the passed member is enabled for all the passed modifiers
+	 * @param member The member to test
+	 * @param modifiers The modifiers to test for
+	 * @return true if the passed member is enabled for all the passed modifiers, false otherwise
+	 */
+	public static boolean isEnabledForAll(final Member member, final SModifier...modifiers) {
+		if(member==null) throw new IllegalArgumentException("The passed member was null");
+		final int mmask = member.getModifiers();
+		for(SModifier m: modifiers) {
+			if(!m.isEnabled(mmask)) return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Determines if the passed member is enabled for any of the passed modifiers
+	 * @param member The member to test
+	 * @param modifiers The modifiers to test for
+	 * @return true if the passed member is enabled for any of the passed modifiers, false otherwise
+	 */
+	public static boolean isEnabledForAny(final Member member, final SModifier...modifiers) {
+		if(member==null) throw new IllegalArgumentException("The passed member was null");
+		final int mmask = member.getModifiers();
+		for(SModifier m: modifiers) {
+			if(!m.isEnabled(mmask)) return true;
+		}
+		return false;
+	}
+	
+	
+	/**
+	 * Determines if the passed mask is enabled for this SModifier
+	 * @param mask The mask to test
+	 * @return true if enabled, false otherwise
+	 */
+	public boolean isEnabled(final int mask) {
+		return mask == (mask | _mask);
+	}
+	
+	/**
+	 * Determines if the passed member is enabled for this SModifier
+	 * @param member The member to test
+	 * @return true if enabled, false otherwise
+	 */
+	public boolean isEnabled(final Member member) {
+		final int m = member.getModifiers();
+		return m == (m | _mask);
+	}
+	
+	
+	
+}
